acc56d16ce60544103bf8b73951ffb55f118d85f
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-12-22 06:29:38 -0300

    Terminando estudos sobre Redux Thunk e chamadas de API

--- trato-tech/src/main.jsx
+++ trato-tech/src/main.jsx
@@ -4,8 +4,11 @@ import './index.css';
 import Router from './routes';
 import store from './store';
 import { Provider } from 'react-redux';
+import { createStandaloneToast } from '@chakra-ui/toast';
+const { ToastContainer, toast } = createStandaloneToast();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <Router />
+    <ToastContainer />
   </Provider>,
 );
--- trato-tech/src/store/reducers/categorias.js
+++ trato-tech/src/store/reducers/categorias.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import categoriasService from '../../services/categorias';
-
+import { createStandaloneToast } from '@chakra-ui/toast';
+import { resetarCarrinho } from './carrinho';
+const { ToastContainer, toast } = createStandaloneToast();
 const initialState = [];
 
 export const buscarCategorias = createAsyncThunk(
@@ -14,14 +16,41 @@ const categoriasSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(buscarCategorias.fulfilled, (_, { payload }) => {
-        console.log('Categorias carregado.');
+        toast({
+          title: 'Sucesso!',
+          description: 'Categorias carregadas com sucesso!',
+          status: 'success',
+          duration: 2000,
+          isClosable: true,
+        });
         return payload;
       })
       .addCase(buscarCategorias.pending, (_, { payload }) => {
-        console.log('Carregando categorias');
+        toast({
+          title: 'Carregando...!',
+          description: 'Categorias carregadas com sucesso!',
+          status: 'loading',
+          duration: 2000,
+          isClosable: true,
+        });
       })
       .addCase(buscarCategorias.rejected, (_, { payload }) => {
-        console.log('Buscar  categorias rejeitada');
+        toast({
+          title: 'Algo falhou!',
+          description: 'Falha ao chamar API!',
+          status: 'error',
+          duration: 2000,
+          isClosable: true,
+        });
+      })
+      .addCase(resetarCarrinho.type, () => {
+        toast({
+          title: 'Sucesso!',
+          description: 'Compra realizada com sucesso!',
+          status: 'success',
+          duration: 2000,
+          isClosable: true,
+        });
       });
   },
 });
